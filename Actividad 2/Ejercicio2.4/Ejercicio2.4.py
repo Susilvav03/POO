1b62ff40c8281f0e3d19ce57a72b22bde86bf7e0
joselol99 <jbolemos@unal.edu.co>
2025-05-05 19:10:20 -0500

    Agregando clase PruebaFiguras

--- Actividad 2/Ejercicio2.4/Ejercicio2.4.py
+++ Actividad 2/Ejercicio2.4/Ejercicio2.4.py
@@ -54,25 +54,30 @@ class TrianguloRectangulo:
         else:
             print("Es un triángulo isósceles")
 
-# Clase de prueba
+class PruebaFiguras:
+    def main(self):
+        figura1 = Circulo(2)
+        figura2 = Rectangulo(1, 2)
+        figura3 = Cuadrado(3)
+        figura4 = TrianguloRectangulo(3, 5)
+
+        print("El área del círculo es =", figura1.calcular_area())
+        print("El perímetro del círculo es =", figura1.calcular_perimetro())
+        print()
+
+        print("El área del rectángulo es =", figura2.calcular_area())
+        print("El perímetro del rectángulo es =", figura2.calcular_perimetro())
+        print()
+
+        print("El área del cuadrado es =", figura3.calcular_area())
+        print("El perímetro del cuadrado es =", figura3.calcular_perimetro())
+        print()
+
+        print("El área del triángulo es =", figura4.calcular_area())
+        print("El perímetro del triángulo es =", figura4.calcular_perimetro())
+        figura4.determinar_tipo_triangulo()
+
+# Ejecutar prueba
 if __name__ == "__main__":
-    figura1 = Circulo(2)
-    figura2 = Rectangulo(1, 2)
-    figura3 = Cuadrado(3)
-    figura4 = TrianguloRectangulo(3, 5)
-
-    print("El área del círculo es =", figura1.calcular_area())
-    print("El perímetro del círculo es =", figura1.calcular_perimetro())
-    print()
-
-    print("El área del rectángulo es =", figura2.calcular_area())
-    print("El perímetro del rectángulo es =", figura2.calcular_perimetro())
-    print()
-
-    print("El área del cuadrado es =", figura3.calcular_area())
-    print("El perímetro del cuadrado es =", figura3.calcular_perimetro())
-    print()
-
-    print("El área del triángulo es =", figura4.calcular_area())
-    print("El perímetro del triángulo es =", figura4.calcular_perimetro())
-    figura4.determinar_tipo_triangulo()
+    prueba = PruebaFiguras()
+    prueba.main()
